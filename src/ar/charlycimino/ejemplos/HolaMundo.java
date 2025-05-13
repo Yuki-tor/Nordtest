4f5c3a66e4f18da70b8fbebf172eaa1f8315efeb
yukiel <ytorres@arkusnexus.com>
2025-05-13 16:15:15 -0700

    Replace mundo with test

--- src/ar/charlycimino/ejemplos/HolaMundo.java
+++ src/ar/charlycimino/ejemplos/HolaMundo.java
@@ -10,7 +10,7 @@ package ar.charlycimino.ejemplos;
 public class HolaMundo {
 
     public static void main(String[] args) {
-        System.out.println("Hola mundo");
+        System.out.println("Hola test");
     }
 
 }
